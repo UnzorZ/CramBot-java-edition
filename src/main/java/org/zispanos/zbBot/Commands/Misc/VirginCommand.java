85847cb6d234736a4e36a52f2a40d015bebaa08d
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2021-07-05 06:41:12 -0700

    Added Virgin Command

---
+++ src/main/java/org/zispanos/zbBot/Commands/Misc/VirginCommand.java
@@ -0,0 +1,32 @@
+package org.zispanos.zbBot.Commands.Misc;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.zispanos.zbBot.Objects.ICommand;
+import org.zispanos.zbBot.Utils.FactsUtil;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.List;
+
+public class VirginCommand implements ICommand{
+    @Override
+    public void handle(List<String> args, GuildMessageReceivedEvent event) throws IOException {
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.setDescription(FactsUtil.getPorcentajes() + " virgin.");
+        embed.setAuthor(event.getAuthor().getAsTag(), null, event.getAuthor().getAvatarUrl());
+        embed.setColor(Color.pink);
+        event.getChannel().sendMessage(embed.build()).queue();
+    }
+
+    @Override
+    public String getHelp() {
+        return null;
+    }
+
+    @Override
+    public String getInvoke() {
+        return "virgin";
+    }
+}
