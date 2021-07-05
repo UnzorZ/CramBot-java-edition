b79eeae41ee323ef6228e96540a9715a4e8ca390
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2021-07-05 06:12:56 -0700

    Fixed getter method

--- src/main/java/org/zispanos/zbBot/Utils/FactsUtil.java
+++ src/main/java/org/zispanos/zbBot/Utils/FactsUtil.java
@@ -8,12 +8,7 @@ public class FactsUtil {
     private static int prevNum;
     private static String donatormotd;
     private static String dadjoke;
-    private static String penis;
-    private static String cope;
-    private static String based;
-    private static String virgin;
-    private static String cramfag;
-    private static String uwunzorfan;
+    private static String porcentajes;
 
 
     public static String getFact() {
@@ -43,14 +38,14 @@ public class FactsUtil {
         dadjoke = Constants.dadjoke.get(randomNum);
         return dadjoke;
     }
-    public static String getPenis() {
+    public static String getPorcentajes() {
         randomNum = (int) (Math.random() * (100));
         if (randomNum == prevNum) {
             randomNum = (int) (Math.random() * (100));
         }
         prevNum = randomNum;
-        penis = Constants.penis.get(randomNum);
-        return penis;
+        porcentajes = Constants.penis.get(randomNum);
+        return porcentajes;
     }
         public static void Load(){
 
