5ed7ca6b4a98567b5278e25b8a70417962888059
UnzorZ <marcoscanal266@gmail.com>
2021-07-26 12:49:39 +0200

    Deleted verify command

--- src/main/java/org/zispanos/zbBot/Commands/Administration/VerifyCommand.java
+++ src/main/java/org/zispanos/zbBot/Commands/Administration/VerifyCommand.java
@@ -17,22 +17,11 @@ public class VerifyCommand implements ICommand{
     public void handle(List<String> args, GuildMessageReceivedEvent event) throws IOException {
         for (String role : Constants.OProles) {
             if (MemberUtil.getRole(Objects.requireNonNull(event.getMember()), role) != null) {
-
                 hasPerms = true;
-
             }
         }
 
-        if (hasPerms) {
-
-            EmbedBuilder embed = new EmbedBuilder();
-
-            embed.setTitle("Verificación");
-            embed.addField("Para verificarte, por favor, reacciona a este mensaje", "Al reaccionar, aceptas que has leido <#536624573505077268> y estas de acuerdo con ellas. Si reaccionas y no ocurre nada, por favor contacta con algun administrador", true);
-            embed.setColor(Color.GREEN);
-            event.getChannel().sendMessage(embed.build()).queue();
-
-        } else {
+        if (!hasPerms){
 
             EmbedBuilder noperms = new EmbedBuilder();
             noperms.setTitle("No tienes permisos para ejecutar este comando");
@@ -41,6 +30,13 @@ public class VerifyCommand implements ICommand{
             noperms.setThumbnail("https://images-ext-1.discordapp.net/external/9JFZB8ciu6xnUwZ6_oPp3FBJ_jnM1WBillvAvxaTYjc/%3Fsize%3D256/https/cdn.discordapp.com/avatars/812711414212722719/0c0cc2e334708a2e8ac28b6cc73747b2.png");
             event.getChannel().sendMessage(noperms.build()).queue(message -> message.addReaction("🗑️").queue());
 
+        } else{
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("Verificación");
+            embed.addField("Para verificarte, por favor, reacciona a este mensaje", "Al reaccionar, aceptas que has leido <#536624573505077268> y estas de acuerdo con ellas. Si reaccionas y no ocurre nada, por favor contacta con algun administrador", true);
+            embed.setColor(Color.GREEN);
+            event.getChannel().sendMessage(embed.build()).queue();
         }
 
     }
