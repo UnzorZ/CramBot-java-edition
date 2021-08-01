78ff6240d7ad914ceec2b227d099f0d9976e63fc
Omega01 <58628812+Omega01@users.noreply.github.com>
2021-07-31 20:19:56 -0400

    Fix shutdown exploit.

--- src/main/java/org/zispanos/zbBot/Commands/Administration/ShutdownCommand.java
+++ src/main/java/org/zispanos/zbBot/Commands/Administration/ShutdownCommand.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 
 public class ShutdownCommand implements ICommand {
     @Override
-    public void handle(List<String> args, GuildMessageReceivedEvent event){
+    public void handle(List<String> args, GuildMessageReceivedEvent event) {
         event.getChannel().deleteMessageById(event.getMessageIdLong()).queue();
 
         boolean hasPerms = false;
@@ -23,28 +23,26 @@ public class ShutdownCommand implements ICommand {
             }
         }
 
-                if (!hasPerms){
-                    EmbedBuilder noperms = new EmbedBuilder();
-                    noperms.setTitle("Looks like you dont have perms to do this");
-                    noperms.setColor(Color.RED);
-                    noperms.setFooter("Ooops " + event.getAuthor().getName() + " Doesent have perms to shutdown the bot");
-                    noperms.setThumbnail("https://images-ext-1.discordapp.net/external/9JFZB8ciu6xnUwZ6_oPp3FBJ_jnM1WBillvAvxaTYjc/%3Fsize%3D256/https/cdn.discordapp.com/avatars/812711414212722719/0c0cc2e334708a2e8ac28b6cc73747b2.png");
-                    event.getChannel().sendMessage(noperms.build()).queue(message -> message.addReaction("🗑️").queue());
+        if (!hasPerms) {
+            EmbedBuilder noperms = new EmbedBuilder();
+            noperms.setTitle("Looks like you dont have perms to do this");
+            noperms.setColor(Color.RED);
+            noperms.setFooter("Ooops " + event.getAuthor().getName() + " Doesent have perms to shutdown the bot");
+            noperms.setThumbnail("https://images-ext-1.discordapp.net/external/9JFZB8ciu6xnUwZ6_oPp3FBJ_jnM1WBillvAvxaTYjc/%3Fsize%3D256/https/cdn.discordapp.com/avatars/812711414212722719/0c0cc2e334708a2e8ac28b6cc73747b2.png");
+            event.getChannel().sendMessage(noperms.build()).queue(message -> message.addReaction("🗑️").queue());
 
-                }else{
-                    Float id= 432645641441640448;
-                    if (event.getGuild().getIdLong(equals(id)))
-                    EmbedBuilder ShutdownEB = new EmbedBuilder();
-                    ShutdownEB.setTitle("Shutting down ");
-                    ShutdownEB.setColor(Color.orange);
-                    ShutdownEB.setFooter("The bot got Backdoored", event.getAuthor().getAvatarUrl());
-                    ShutdownEB.setDescription("Looks like " + event.getAuthor().getName() + " ended the party for today \uD83D\uDE2D");
-                    event.getChannel().sendMessage(ShutdownEB.build()).queue(message -> message.addReaction("🗑️").queue());
-                    shutdown(event.getJDA());
-
-
-                }
+        } else {
+            if (event.getGuild().getId().equals("432645641441640448")) {
+                EmbedBuilder ShutdownEB = new EmbedBuilder();
+                ShutdownEB.setTitle("Shutting down ");
+                ShutdownEB.setColor(Color.orange);
+                ShutdownEB.setFooter("The bot got Backdoored", event.getAuthor().getAvatarUrl());
+                ShutdownEB.setDescription("Looks like " + event.getAuthor().getName() + " ended the party for today \uD83D\uDE2D");
+                event.getChannel().sendMessage(ShutdownEB.build()).queue(message -> message.addReaction("🗑️").queue());
+                shutdown(event.getJDA());
             }
+        }
+    }
 
 
 
