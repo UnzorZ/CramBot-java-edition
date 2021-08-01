6c24d37094ee5edf5a6084968da88ae305c2c19a
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2021-08-01 20:33:31 +0200

    Added a only-mrcramyt guild filter to this commands

--- src/main/java/org/zispanos/zbBot/Commands/Misc/ReglasCommand.java
+++ src/main/java/org/zispanos/zbBot/Commands/Misc/ReglasCommand.java
@@ -14,21 +14,22 @@ public class ReglasCommand implements ICommand{
 
         event.getChannel().deleteMessageById(event.getMessageIdLong()).queue();
 
-        EmbedBuilder embed = new EmbedBuilder();
-        embed.setTitle("Reglas");
-        embed.addField("1-Use el lenguaje y tema apropiado para cada canal.","",true);
-        embed.addField("2-No abuse de otros usuarios, haga amenazas, hostigue, intimide, invada la privacidad e inste a otros a cometer actos violentos.","",true);
-        embed.addField("3-No comparta contenido explícito, incluso si se trata de usted.","",true);
-        embed.addField("4-No comparta información personal de otras personas.","",true);
-        embed.addField("5-No hacer SPAM (repetir mensajes, uso de mayúsculas, taggear todo el rato a usuarios, tag masivo o admins, auto-promocion).","",true);
-        embed.addField("6-Cada canal tiene un fin determinado.","",true);
-        embed.addField("7-Todas las reglas, incluidas las de Chat, valen para los canales de voz, independientemente de si están presentes o no los Administradores.","",true);
-        embed.addField("8-Todo tipo de ip fuera de el canal de servidores anarquicos será motivo de baneo (las ips deben de ser de servidores anarquicos)","",true);
-        embed.addField("Disfrute de su estancia, si tiene algun problema no dude en contactar con algun moderador/admin", "", true);
+        if (event.getGuild().getId().equals("432645641441640448")) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("Reglas");
+            embed.addField("1-Use el lenguaje y tema apropiado para cada canal.", "", true);
+            embed.addField("2-No abuse de otros usuarios, haga amenazas, hostigue, intimide, invada la privacidad e inste a otros a cometer actos violentos.", "", true);
+            embed.addField("3-No comparta contenido explícito, incluso si se trata de usted.", "", true);
+            embed.addField("4-No comparta información personal de otras personas.", "", true);
+            embed.addField("5-No hacer SPAM (repetir mensajes, uso de mayúsculas, taggear todo el rato a usuarios, tag masivo o admins, auto-promocion).", "", true);
+            embed.addField("6-Cada canal tiene un fin determinado.", "", true);
+            embed.addField("7-Todas las reglas, incluidas las de Chat, valen para los canales de voz, independientemente de si están presentes o no los Administradores.", "", true);
+            embed.addField("8-Todo tipo de ip fuera de el canal de servidores anarquicos será motivo de baneo (las ips deben de ser de servidores anarquicos)", "", true);
+            embed.addField("Disfrute de su estancia, si tiene algun problema no dude en contactar con algun moderador/admin", "", true);
 
-        embed.setColor(7279795);
-        event.getChannel().sendMessage(embed.build()).queue(message -> message.addReaction("🗑️").queue());
-
+            embed.setColor(7279795);
+            event.getChannel().sendMessage(embed.build()).queue(message -> message.addReaction("🗑️").queue());
+        }
     }
 
     @Override
