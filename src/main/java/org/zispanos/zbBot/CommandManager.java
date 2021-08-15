178878c64c989955e7b905768e36c8705d985000
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2021-08-15 19:10:12 +0200

    Update CommandManager.java

--- src/main/java/org/zispanos/zbBot/CommandManager.java
+++ src/main/java/org/zispanos/zbBot/CommandManager.java
@@ -53,6 +53,16 @@ public class CommandManager {
         addCommand(new UwunzorfagCommand());
         addCommand(new InviteCommand());
         addCommand(new getHelpCommand());
+        addCommand(new EnglishCommand());
+        addCommand(new DebugCommand());
+        addCommand(new SayCommand());
+        addCommand(new BallCommand());
+        addCommand(new EmbedCreatorCommand());
+        addCommand(new setRPCCommand());
+        addCommand(new BanCommand());
+        addCommand(new PreguntadeldiaCommand());
+        addCommand(new KickCommand());
+        addCommand(new FurryNSFWCommand());
     }
 
     private void addCommand(ICommand command) {
