0279ead7bcb45f4a62de3c48c180192a1028e82e
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2021-09-14 19:58:31 +0200

    Added furry command (%)

--- src/main/java/org/zispanos/zbBot/Commands/Misc/FurryCommand.java
+++ src/main/java/org/zispanos/zbBot/Commands/Misc/FurryCommand.java
@@ -1,7 +1,10 @@
 package org.zispanos.zbBot.Commands.Misc;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.zispanos.zbBot.Objects.ICommand;
+import org.zispanos.zbBot.Utils.FactsUtil;
+import org.zispanos.zbBot.Utils.RandomColorUtil;
 
 import java.io.IOException;
 import java.util.List;
@@ -10,10 +13,12 @@ import java.util.List;
 public class FurryCommand implements ICommand{
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent event) throws InterruptedException, IOException {
+        EmbedBuilder embed = new EmbedBuilder();
 
-        event.getChannel().deleteMessageById(event.getMessageIdLong()).queue();
-        event.getChannel().sendMessage("https://tenor.com/view/jeramy-boyd-boyd-jeramy-bear-funny-gif-13137533").queue();
-
+        embed.setDescription(FactsUtil.getPorcentajes() + " furry.");
+        embed.setAuthor(event.getAuthor().getAsTag(), "https://www.youtube.com/watch?v=dQw4w9WgXcQ", event.getAuthor().getAvatarUrl());
+        embed.setColor(RandomColorUtil.getRandomColor());
+        event.getChannel().sendMessage(embed.build()).queue();
     }
 
     @Override
