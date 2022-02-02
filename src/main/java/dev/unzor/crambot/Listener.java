ce561abc48a57668b12a3a95431ec08ecc7b0a78
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2022-02-02 18:16:42 +0100

    Added anti-zoophile system

--- src/main/java/dev/unzor/crambot/Listener.java
+++ src/main/java/dev/unzor/crambot/Listener.java
@@ -1,10 +1,12 @@
-package org.zispanos.zbBot;
+package dev.unzor.crambot;
 
+import dev.unzor.crambot.Objects.Constants;
+import dev.unzor.crambot.Utils.RandomColorUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -12,13 +14,10 @@ import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.zispanos.zbBot.Objects.Constants;
-import org.zispanos.zbBot.Utils.RandomColorUtil;
 
 import java.awt.*;
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
@@ -34,6 +33,7 @@ class Listener extends ListenerAdapter {
     @Override
     public void onReady(ReadyEvent event) {
         logger.info(String.format("Logged in as %#s", event.getJDA().getSelfUser()));
+        logger.info(String.format("Im currently in " + event.getJDA().getGuilds().size()) + " guilds");
     }
 
     @Override
@@ -76,6 +76,8 @@ class Listener extends ListenerAdapter {
             EmbedBuilder embed = new EmbedBuilder();
             embed.setTitle("No esta permitido decir esta palabra en este servidor " + author.getName());
             embed.setColor(Color.RED);
+
+            User user;
 
 
             if (msg.contains(".aternos".toLowerCase(Locale.ROOT))) {
@@ -157,6 +159,33 @@ class Listener extends ListenerAdapter {
                 event.getChannel().sendMessage(embed.build()).queue();
                 return;
             }
+            if (msg.contains("卐")) {
+                event.getChannel().deleteMessageById(event.getMessageIdLong()).queue();
+            }
+            if (msg.contains("ζ")) {
+                String name = event.getAuthor().getName();
+                EmbedBuilder embed2 = new EmbedBuilder();
+                embed2.setTitle("Detected Zoophile, banning them...");
+                embed2.setColor(Color.RED);
+                embed2.setFooter("Succesfully banned " + name, event.getAuthor().getAvatarUrl());
+                event.getChannel().sendMessage(embed2.build()).queue();
+                user = event.getAuthor();
+                event.getGuild().ban(user, 0, "Zoophile symbol found in its message").queue();
+                event.getChannel().deleteMessageById(event.getMessageIdLong()).queue();
+            }
+            String name = event.getAuthor().getName();
+            Member member = event.getMember();
+
+            if (name.contains("ζ")) {
+                assert member != null;
+                member.ban(0, "Zoophile symbol found in its name").complete();
+                EmbedBuilder embed2 = new EmbedBuilder();
+                embed2.setTitle("Detected Zoophile, banning them...");
+                embed2.setColor(Color.RED);
+                embed2.setFooter("Succesfully banned " + name, event.getAuthor().getAvatarUrl());
+                event.getChannel().sendMessage(embed2.build()).queue();
+            }
+
         }
         Calendar c = Calendar.getInstance();
         c.setTimeInMillis(System.currentTimeMillis() - System.currentTimeMillis());
@@ -169,6 +198,7 @@ class Listener extends ListenerAdapter {
             mDay = 0;
         }
     }
+
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 
@@ -181,5 +211,18 @@ class Listener extends ListenerAdapter {
                 e.printStackTrace();
             }
         }
+
+        long id = event.getJDA().getSelfUser().getIdLong();
+        String msg = event.getMessage().getContentRaw();
+
+        if (msg.contains("<@" + id + ">")) {
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("Hola, veo que me has llamado");
+            embed.setDescription("Mi prefix es " + Constants.PREFIX + ", y si necesitas ayuda puedes visitar mi [página web](https://crambot.xyz/)");
+            embed.setColor(RandomColorUtil.getRandomColor());
+            event.getChannel().sendMessage(embed.build()).queue();
+        }
+
     }
 }
